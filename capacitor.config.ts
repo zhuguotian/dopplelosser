8f94f3ecc89f9ad05769f297bfa3fb10aec9dd52
Guotian Zhu <guotianzhu@ctamericas.com>
2026-07-21 16:59:34 -0400

    fix: switch capacitor config to json, avoid needing typescript in CI

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dopplelosser.shadowdiary',
-  appName: '影子日记',
-  webDir: 'www'
-};
-
-export default config;
